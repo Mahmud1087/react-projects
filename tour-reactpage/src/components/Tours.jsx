5647993de77e96759a9f47e359da5e2c49302491
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-02 06:43:23 +0100

    fix: made changes to the header

--- tour-reactpage/src/components/Tours.jsx
+++ tour-reactpage/src/components/Tours.jsx
@@ -3,8 +3,10 @@ import Tour from './Tour'
 const Tours = ({ tour, setTour }) => {
   return (
     <>
-      <h2>Our Tours</h2>
-      <div className='underline'></div>
+      <div className='title'>
+        <h2>Our Tours</h2>
+        <div className='underline'></div>
+      </div>
       <Tour tour={tour} setTour={setTour} />
     </>
   )
