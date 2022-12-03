029588cb9d9588ef3dccacde629ec6317718d759
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-03 07:21:58 +0100

    feat: Implemented the previous button, next button and the suprise me button

--- review-reactpage/src/App.jsx
+++ review-reactpage/src/App.jsx
@@ -1,8 +1,15 @@
-import Review from './Review'
+import Review from './components/Review'
+
 function App() {
   return (
     <main>
-      <Review />
+      <section className='container'>
+        <div className='title'>
+          <h2>Our Reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
     </main>
   )
 }
--- review-reactpage/src/components/Review.jsx
+++ review-reactpage/src/components/Review.jsx
@@ -1,9 +1,66 @@
 import { useState } from 'react'
 import people from './data'
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+import reactLogo from '../assets/react.svg'
 
 const Review = () => {
-  return <h2>review component</h2>
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+
+  function checkNumber(number) {
+    if (number < 0) return people.length - 1
+    if (number > people.length - 1) return 0
+    return number
+  }
+
+  function nextBtn() {
+    setIndex((prevIndex) => {
+      let newIndex = prevIndex + 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  function prevBtn() {
+    setIndex((prevIndex) => {
+      let newIndex = prevIndex - 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  function random() {
+    let randomNumber = Math.floor(Math.random() * people.length)
+    if (randomNumber === index) randomNumber = index + 1
+    setIndex(checkNumber(randomNumber))
+  }
+
+  return (
+    <>
+      <div className='review'>
+        <div className='img-container'>
+          <img src={reactLogo} className='person-img' />
+          <div className='quote-icon'>
+            <FaQuoteRight />
+          </div>
+        </div>
+
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+
+        <div className='button-container'>
+          <button className='prev-btn' onClick={prevBtn}>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' onClick={nextBtn}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className='random-btn' onClick={random}>
+          suprise me
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default Review
