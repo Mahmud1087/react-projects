15230c2b9d637f12b6c3360de38aa14b6f8947bc
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-08 11:44:52 +0100

    feat: added a new unique array as state and passed down to the categories.jsx component

--- food-menu-reactpage/src/App.jsx
+++ food-menu-reactpage/src/App.jsx
@@ -3,14 +3,19 @@ import Menu from './components/Menu'
 import Categories from './components/Categories'
 import items from './components/data'
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
 function App() {
   const [foodItems, setFoodItems] = useState(items)
-  const [lunch, setLunch] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
 
   function filterMenu(cat) {
-    setFoodItems((prevFood) =>
-      prevFood.filter((foodMenu) => foodMenu.category === cat)
-    )
+    if (cat === 'all') {
+      setFoodItems(items)
+      return
+    }
+    const newFilter = items.filter((item) => item.category === cat)
+    setFoodItems(newFilter)
   }
 
   return (
@@ -20,7 +25,7 @@ function App() {
           <h2>Our menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories filterMenu={filterMenu} />
+        <Categories filterMenu={filterMenu} categories={categories} />
         <Menu foodItems={foodItems} />
       </div>
     </main>
