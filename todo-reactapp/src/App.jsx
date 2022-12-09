f412f05b310a6a4cbf1e2a02a272adc9dd70fcee
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-09 06:06:00 +0100

    imported the ThemeProvider for the theme functionality, the Global styles and the colors.jsx

--- todo-reactapp/src/App.jsx
+++ todo-reactapp/src/App.jsx
@@ -1,3 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { colors } from './components/colors'
+import { GlobalStyles } from './components/styles/Global.styled'
+
 export default function App() {
-  return <div>App</div>
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const theme = {
+    body: isDarkMode ? colors.veryDarkBlue : colors.veryLightGrayishBlue,
+    header_ItemBoxBG: isDarkMode
+      ? colors.veryDarkDesaturatedBlue
+      : colors.veryLightGray,
+    itemTexts: isDarkMode
+      ? colors.lightGrayishBlue2
+      : colors.veryDarkGrayishBlue,
+    itemCompleted: isDarkMode
+      ? colors.DarkGrayishBlue2
+      : colors.DarkGrayishBlue,
+    checkerBorder_ItemBottomBorder: isDarkMode
+      ? colors.veryDarkGrayishBlue
+      : colors.lightGrayishBlue,
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <h2>Hello</h2>
+    </ThemeProvider>
+  )
 }
