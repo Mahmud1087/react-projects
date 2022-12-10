0694cde699d7823f3cd77ed9b4711238d4188203
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-10 04:22:42 +0100

    added the styles for the TodoBody.jsx component

---
+++ todo-reactapp/src/components/styles/TodoBody.styled.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const BoxItem = styled.div`
+  padding: 0.8rem 1.2rem;
+  background-color: ${({ theme }) => theme.header_ItemBoxBG};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 6px;
+`
+
+export const CreateItemInput = styled.input`
+  background: transparent;
+  border: none;
+  color: ${({ theme }) => theme.itemTexts};
+  width: 95%;
+  padding: 0.3rem 0;
+  font-weight: 600;
+  font-family: 'Josefin Sans';
+  font-size: 0.9rem;
+
+  &:focus {
+    outline: none;
+  }
+`
+
+export const AddButton = styled.button`
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+  border-radius: 50px;
+  color: ${({ theme }) => theme.blue};
+
+  &:focus {
+    outline: none;
+  }
+`
