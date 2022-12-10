700336e0fc2c8d35f0b93d950b7219db51336d66
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-10 04:19:10 +0100

    added the styles corresponding to the App.jsx

---
+++ todo-reactapp/src/components/styles/App.styled.jsx
@@ -0,0 +1,88 @@
+import styled from 'styled-components'
+
+export const LightModeBgMobile = styled.img`
+  display: none;
+
+  @media screen and (min-width: 320px) and (max-width: 480px) {
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -1;
+  }
+`
+
+export const LightModeBgDesktop = styled.img`
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -1;
+  height: 30%;
+  object-fit: cover;
+
+  @media screen and (min-width: 320px) and (max-width: 480px) {
+    display: none;
+  }
+`
+
+export const DarkModeBgMobile = styled.img`
+  display: none;
+
+  @media screen and (min-width: 320px) and (max-width: 480px) {
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -1;
+  }
+`
+
+export const DarkModeBgDesktop = styled.img`
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -1;
+  height: 30%;
+  object-fit: cover;
+
+  @media screen and (min-width: 320px) and (max-width: 480px) {
+    display: none;
+  }
+`
+
+export const Container = styled.div`
+  min-width: 70%;
+  width: 90%;
+  margin: auto;
+`
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 3rem 0 2.3rem;
+`
+
+export const Heading = styled.h2`
+  letter-spacing: 13px;
+  font-weight: 700;
+  color: hsl(0, 0%, 98%);
+`
+
+export const LightModeIcon = styled.img`
+  width: 1.3rem;
+`
+
+export const DarkModeIcon = styled.img`
+  width: 1.3rem;
+`
+
+export const Footer = styled.footer`
+  margin: 3rem 0;
+  color: ${({ theme }) => theme.itemCompleted};
+  font-weight: 600;
+  font-size: 1.15rem;
+  text-align: center;
+`
