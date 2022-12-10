1fb25cbe1a123f7cbe072d198608517971c6dc92
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-10 11:59:50 +0100

    styles for Modal.styled.jsx

---
+++ todo-reactapp/src/components/styles/Modal.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const PopUpModal = styled.div`
+  position: absolute;
+  top: 0.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  color: orange;
+  background: rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+  padding: 0.5rem 1.2rem;
+  text-transform: capitalize;
+  font-weight: 600;
+  transition: all 0.4s ease-in-out;
+`
