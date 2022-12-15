795fd9aec8bd1c54e0b7302f8bd3b9784b317a62
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-15 14:28:51 +0100

    implemented the all, active and completed buttons

--- todo-reactapp/src/components/reducer.jsx
+++ todo-reactapp/src/components/reducer.jsx
@@ -2,14 +2,18 @@ export const reducer = (state, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
       const newItem = [...state.todoList, action.payload]
+      const originalItem = newItem
       return {
         ...state,
         todoList: newItem,
+        todoListCopy: originalItem,
         isModalOpen: true,
         modalContent: 'task added',
       }
+
     case 'NO_ITEM':
       return { ...state, isModalOpen: true, modalContent: 'no task entered' }
+
     case 'REMOVE_ITEM':
       const newItems = state.todoList.filter(
         (item) => item.id !== action.payload
@@ -17,11 +21,14 @@ export const reducer = (state, action) => {
       return {
         ...state,
         todoList: newItems,
+        todoListCopy: newItems,
         isModalOpen: true,
         modalContent: 'task removed',
       }
+
     case 'CLOSE_MODAL':
       return { ...state, isModalOpen: false }
+
     case 'COMPLETED_TASK':
       const completedTask = state.todoList.map((task) =>
         task.id === action.payload
@@ -34,7 +41,9 @@ export const reducer = (state, action) => {
       return {
         ...state,
         todoList: completedTask,
+        todoListCopy: completedTask,
       }
+
     case 'CLEAR_COMPLETED':
       const newTasks = state.todoList.filter(
         (task) => task.isCompleted !== action.payload
@@ -42,6 +51,22 @@ export const reducer = (state, action) => {
       return {
         ...state,
         todoList: newTasks,
+        todoListCopy: newTasks,
+      }
+
+    case 'ACTIVE_TASK':
+      const activeTasks = state.todoListCopy.filter((task) => {
+        if (action.payload === 'active') {
+          return task.isCompleted !== true
+        } else if (action.payload === 'completed') {
+          return task.isCompleted !== false
+        } else {
+          return task
+        }
+      })
+      return {
+        ...state,
+        todoList: activeTasks,
       }
     default:
       throw new Error('No matching type')
