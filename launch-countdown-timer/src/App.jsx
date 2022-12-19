a74d1e70cb347385682b90d2b40dcf59ddfc3c46
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-19 13:59:20 +0100

    added the footer component and the title text

--- launch-countdown-timer/src/App.jsx
+++ launch-countdown-timer/src/App.jsx
@@ -1,5 +1,10 @@
 import Footer from './components/Footer'
 
 export default function App() {
-  return <Footer />
+  return (
+    <main>
+      <h2 className='title'>we're launching soon</h2>
+      <Footer />
+    </main>
+  )
 }
