5b2b3b826dfef3835ded4d2cb2e396c64f9d8a05
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-19 13:59:54 +0100

    added the footer component

--- launch-countdown-timer/src/components/Footer.jsx
+++ launch-countdown-timer/src/components/Footer.jsx
@@ -1,11 +1,20 @@
+import { FaFacebookSquare, FaInstagram, FaPinterest } from 'react-icons/fa'
+
 export default function Footer() {
   return (
-    <div className='attribution'>
-      Challenge by{' '}
-      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
-        Frontend Mentor
-      </a>
-      . Coded by <a href='#'>Mahmud Abdulazeez</a>.
-    </div>
+    <>
+      <footer>
+        <FaFacebookSquare className='icons' />
+        <FaPinterest className='icons' />
+        <FaInstagram className='icons' />
+      </footer>
+      <div className='attribution'>
+        Challenge by{' '}
+        <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by <a href='#'>Mahmud Abdulazeez</a>.
+      </div>
+    </>
   )
 }
