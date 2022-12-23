807e59cf7fe8cb18170480993773a2a4ef01b4ad
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-24 00:34:19 +0100

    project done

--- launch-countdown-timer/src/App.jsx
+++ launch-countdown-timer/src/App.jsx
@@ -4,7 +4,53 @@ import SingleColor from './components/SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('')
+  const [isError, setIsError] = useState(false)
+  const [colorList, setColorList] = useState(new Values('#f15025').all(10))
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      const newColors = new Values(color).all(10)
+      setColorList(newColors)
+      setIsError(false)
+    } catch (error) {
+      setIsError(true)
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            placeholder='#f15025'
+            className={`${isError ? 'error' : null}`}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {colorList.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              index={index}
+              hexColor={color.hex}
+              {...color}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
--- launch-countdown-timer/src/components/SingleColor.jsx
+++ launch-countdown-timer/src/components/SingleColor.jsx
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ index, weight, hexColor }) => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleCopyText = () => {
+    navigator.clipboard.writeText(`#${hexColor}`)
+    setIsCopied(!isCopied)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsCopied(false)
+    }, 3000)
+  }, [isCopied])
+
+  const copiedText = () => {}
+  return (
+    <article
+      className={`${index > 10 ? 'color color-light' : 'color'}`}
+      style={{ backgroundColor: `#${hexColor}` }}
+      onClick={handleCopyText}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>#{hexColor}</p>
+      {isCopied && <p className='alert'>copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
