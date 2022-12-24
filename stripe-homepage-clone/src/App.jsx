54c0de10a648c5a5f43741567054f11550415275
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-24 17:06:54 +0100

    added 2 new projects

---
+++ stripe-homepage-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Sidebar from './components/Sidebar'
+import Submenu from './components/Submenu'
+function App() {
+  return (
+    <>
+      <h2>stripe submenus setup</h2>
+    </>
+  )
+}
+
+export default App
