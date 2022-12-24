7532b5ded66e0b306c4f8185be1d33335281a947
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-24 01:33:10 +0100

    completed the navbar

--- navbar-sidenavbar-modal/src/components/Navbar.jsx
+++ navbar-sidenavbar-modal/src/components/Navbar.jsx
@@ -1,10 +1,60 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { FaBars, FaTwitter } from 'react-icons/fa'
 import { links, social } from './data'
-import logo from './logo.svg'
+import logo from '../assets/logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if (isMenuOpen) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [isMenuOpen])
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='coding addict' className='logo' />
+          <button
+            className='nav-toggle'
+            onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <FaBars />
+          </button>
+        </div>
+        {
+          <div className='links-container' ref={linksContainerRef}>
+            <ul className='links' ref={linksRef}>
+              {links.map((link) => {
+                const { id, text } = link
+                return (
+                  <li key={id}>
+                    <a href='#'>{text}</a>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        }
+        <ul className='social-icons'>
+          {social.map((socials) => {
+            const { id, url, icon } = socials
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
