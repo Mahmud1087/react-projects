c6c075b0195969f0c10430229affcda62919acf4
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-24 18:04:07 +0100

    finised designing the project structure and the sidebar for the mobile menu

--- stripe-homepage-clone/src/components/Sidebar.jsx
+++ stripe-homepage-clone/src/components/Sidebar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const { closeMenu, isMenuOpen } = useGlobalContext()
+
+  return (
+    <div
+      className={`${isMenuOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'}`}>
+      <aside className='sidebar'>
+        <button className='close-btn' onClick={closeMenu}>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((sublink, index) => {
+            const { page, links } = sublink
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link, index) => {
+                    const { label, icon, url } = link
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </aside>
+    </div>
+  )
 }
 
 export default Sidebar
--- stripe-homepage-clone/src/components/context.jsx
+++ stripe-homepage-clone/src/components/context.jsx
@@ -1,2 +1,30 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+export const AppContext = React.createContext()
+
+export const AppProvider = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const openMenu = () => {
+    setIsMenuOpen(true)
+  }
+  const closeMenu = () => {
+    setIsMenuOpen(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        isMenuOpen,
+        openMenu,
+        closeMenu,
+      }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
