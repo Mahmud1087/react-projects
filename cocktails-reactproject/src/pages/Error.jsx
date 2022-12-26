b1f0df13d6e65d55a936f0ed9a5c44f1a2588b82
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-26 12:27:19 +0100

    feat: edited the 'Error.jsx' page, and implemented the 'About.jsx' page

--- cocktails-reactproject/src/pages/Error.jsx
+++ cocktails-reactproject/src/pages/Error.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <aside className='error-page'>
+    <section className='error-page section'>
       <div className='error-container'>
-        <h3>Oops! It's a dead end</h3>
-        <Link to='/' className='btn-primary'>
+        <h1>oops! it's a dead end</h1>
+        <Link to='/' className='btn btn-primary'>
           back home
         </Link>
       </div>
-    </aside>
+    </section>
   )
 }
 
