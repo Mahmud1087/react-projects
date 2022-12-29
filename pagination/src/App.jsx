2f2af988dd96b6915c2398265ee335bd4e587d8e
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-29 18:32:56 +0100

    project completed

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -2,8 +2,39 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
-  // const []
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
   const { loading, data } = useFetch()
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+
+  function prevPage() {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        return data.length - 1
+      }
+      return prevPage
+    })
+  }
+
+  function nextPage() {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        return 0
+      }
+      return nextPage
+    })
+  }
+
+  function handlePage(index) {
+    setPage(index)
+  }
+
   return (
     <main>
       <div className='section-title'>
@@ -12,10 +43,30 @@ function App() {
       </div>
       <div className='followers'>
         <div className='container'>
-          {data.map((follower) => {
+          {followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />
           })}
         </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
+                  onClick={() => handlePage(index)}>
+                  {index + 1}
+                </button>
+              )
+            })}
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
       </div>
     </main>
   )
