58e5854d1b2643ff989f775fcdc4b80889c110b9
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-29 09:14:27 +0100

    complemeted the follower component

--- pagination/src/useFetch.jsx
+++ pagination/src/useFetch.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import paginate from './utils'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+const url = 'https://api.github.com/users/Mahmud1087/followers?per_page=60'
 
 export const useFetch = () => {
   const [loading, setLoading] = useState(true)
