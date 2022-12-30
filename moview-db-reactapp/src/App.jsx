b19a6272a9dcf652e85444589ebe59a594502c0d
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-30 14:54:42 +0100

    constructed the api url for fetching the data

--- moview-db-reactapp/src/App.jsx
+++ moview-db-reactapp/src/App.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
-import Movie from './SingleMovie'
+import SingleMovie from './SingleMovie'
+// import Movie from './SingleMovie'
+
+// const myKey = 'http://www.omdbapi.com/?i=tt3896198&apikey=1592c060'
 
 function App() {
-  return <h2>movie DB starter</h2>
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movie/:id' element={<SingleMovie />} />
+    </Routes>
+  )
 }
 
 export default App
--- moview-db-reactapp/src/context.jsx
+++ moview-db-reactapp/src/context.jsx
@@ -1,9 +1,21 @@
 import React, { useState, useContext, useEffect } from 'react'
 // make sure to use https
-// export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+export const API_ENDPOINT = `https://www.omdbapi.com/?s=batman&apikey=${
+  import.meta.env.VITE_MOVIE_API_KEY
+}`
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  async function fetchData() {
+    const response = await fetch(API_ENDPOINT)
+    const data = await response.json()
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
 // make sure use
