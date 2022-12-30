cca626d0927c2cdf1aeedc66e50641d59172faa3
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-30 12:17:54 +0100

    completed project and implemented the dark mode feature

--- dark-mode/src/App.jsx
+++ dark-mode/src/App.jsx
@@ -2,12 +2,37 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const localStorageTheme = () => {
+  let theme = 'light-theme'
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
 function App() {
+  const [theme, setTheme] = useState(localStorageTheme())
+
+  const handleToggle = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else {
+      setTheme('light-theme')
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
   return (
     <main>
       <div className='nav-center'>
         <h1>Overreacted</h1>
-        <button className='btn'>toggle</button>
+        <button className='btn' onClick={handleToggle}>
+          toggle
+        </button>
       </div>
       <div className='articles'>
         {data.map((article) => {
