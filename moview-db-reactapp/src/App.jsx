21e8ad487b9dadb3f5443fb6cd216f65ef40660e
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-30 16:16:21 +0100

    feat: implemented the signleMovie page

--- moview-db-reactapp/src/App.jsx
+++ moview-db-reactapp/src/App.jsx
@@ -3,9 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './Home'
 import SingleMovie from './SingleMovie'
-// import Movie from './SingleMovie'
-
-// const myKey = 'http://www.omdbapi.com/?i=tt3896198&apikey=1592c060'
 
 function App() {
   return (
--- moview-db-reactapp/src/SingleMovie.jsx
+++ moview-db-reactapp/src/SingleMovie.jsx
@@ -2,9 +2,55 @@ import React, { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { API_ENDPOINT } from './context'
 
+const url =
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+
 const SingleMovie = () => {
+  const [loading, setLoading] = useState(true)
+  const [movie, setMovie] = useState({})
   const { id } = useParams()
-  return <h2>{id}</h2>
+
+  async function fetchMovie() {
+    const response = await fetch(`${API_ENDPOINT}&i=${id}`)
+    const data = await response.json()
+    setMovie(data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchMovie()
+  }, [])
+
+  const { Title: title, Year: year, Plot: plot, Poster: poster } = movie
+
+  if (loading) {
+    return <div className='loading'></div>
+  }
+
+  if (error.show) {
+    return (
+      <div className='page-error'>
+        <h1>{error.msg}</h1>
+        <Link to='/' className='btn'>
+          back to movies
+        </Link>
+      </div>
+    )
+  }
+
+  return (
+    <section className='single-movie'>
+      <img src={poster === 'N/A' ? url : poster} alt={title} />
+      <div className='single-movie-info'>
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </div>
+    </section>
+  )
 }
 
 export default SingleMovie
