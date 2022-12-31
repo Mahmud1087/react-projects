49b7ed3f4b28fc5c5358aad9823494c0bc24527a
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2022-12-31 11:23:55 +0100

    modal done

--- quiz-reactapp/src/Modal.jsx
+++ quiz-reactapp/src/Modal.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Modal = () => {
-  const { closeModal, isModalOpen } = useGlobalContext()
+  const { closeModal, isModalOpen, correct, questions } = useGlobalContext()
   return (
     <div className={isModalOpen ? 'modal-container isOpen' : 'modal-container'}>
       <div className='modal-content'>
         <h3>congrats!</h3>
+        <p>
+          you answered {((correct / questions.length) * 100).toFixed(0)}% of
+          questions correctly
+        </p>
         <button className='close-btn' onClick={closeModal}>
           play again
         </button>
