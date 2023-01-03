d48f49764947dfeb11a6c577c7101f36ddd49764
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-03 12:41:35 +0100

    added all the components, pages and dependecies

--- github-users-search/src/main.jsx
+++ github-users-search/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { GithubProvider } from './context/context'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
