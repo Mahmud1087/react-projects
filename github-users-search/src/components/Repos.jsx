bd1ec55c2fb216b01f50f6ead1b9f0d59d381fc6
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-05 12:50:45 +0100

    feat: added the reduce function and the column component for the most stars per language

--- github-users-search/src/components/Repos.jsx
+++ github-users-search/src/components/Repos.jsx
@@ -1,29 +1,59 @@
 import React from 'react'
 import styled from 'styled-components'
 import { GithubContext } from '../context/context'
-import { ExampleChart, Pie, Column, Bar, Doughnut } from './Charts'
+import { Pie, Column, Bar, Doughnut } from './Charts'
 const Repos = () => {
   const { repos } = React.useContext(GithubContext)
   let languages = repos.reduce((total, item) => {
-    const { language } = item
+    const { language, stargazers_count } = item
     if (!language) return total
     if (!total[language]) {
-      total[language] = { label: language, value: 1 }
+      total[language] = { label: language, value: 1, stars: stargazers_count }
     } else {
-      total[language] = { ...total[language], value: total[language].value + 1 }
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      }
     }
     return total
   }, {})
+
+  // most used languages
   const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value
     })
     .slice(0, 5)
 
+  // most stars per language
+  const mostStars = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value
+    })
+    .map((item) => {
+      return { ...item, value: item.stars }
+    })
+    .slice(0, 5)
+
+  let { stars } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name } = item
+      total.stars[stargazers_count] = { label: name, value: stargazers_count }
+      return total
+    },
+    { stars: {} }
+  )
+
+  stars = Object.values(stars).slice(-5).reverse()
+
   return (
     <section className='section'>
       <Wrapper className='section-center'>
         <Pie data={mostUsed} />
+        <Column data={stars} />
+        {/* <Doughnut data={mostStars}/>
+        <Bar data={forks}/> */}
       </Wrapper>
     </section>
   )
