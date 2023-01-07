bf46fc831f0074145dd28508007421355b22a475
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-07 11:16:32 +0100

    feat: implemented the search functionality

--- github-users-search/src/components/Search.jsx
+++ github-users-search/src/components/Search.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React, { useContext, useState } from 'react'
+import styled from 'styled-components'
+import { MdSearch } from 'react-icons/md'
+import { GithubContext } from '../context/context'
 const Search = () => {
-  return <h2>search component</h2>;
-};
+  const [user, setUser] = useState('')
+  const { searchGithubUser, request, error, isLoading } =
+    useContext(GithubContext)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!user) {
+      // more logic
+      console.log('no input')
+    } else {
+      // more logic
+      searchGithubUser(user)
+    }
+  }
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input
+              type='text'
+              placeholder='Enter Github User'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            {request > 0 && !isLoading && <button type='submit'>search</button>}
+          </div>
+        </form>
+        <h3>request : {request} / 60</h3>
+      </Wrapper>
+    </section>
+  )
+}
 
 const Wrapper = styled.div`
   position: relative;
@@ -74,7 +111,7 @@ const Wrapper = styled.div`
     color: var(--clr-grey-5);
     font-weight: 400;
   }
-`;
+`
 const ErrorWrapper = styled.article`
   position: absolute;
   width: 90vw;
@@ -86,5 +123,5 @@ const ErrorWrapper = styled.article`
     color: red;
     letter-spacing: var(--spacing);
   }
-`;
-export default Search;
+`
+export default Search
--- github-users-search/src/context/context.jsx
+++ github-users-search/src/context/context.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import mockUser from './mockData.js/mockUser'
 import mockRepos from './mockData.js/mockRepos'
 import mockFollowers from './mockData.js/mockFollowers'
+
 import axios from 'axios'
 
 const rootUrl = 'https://api.github.com'
@@ -13,8 +14,76 @@ const GithubProvider = ({ children }) => {
   const [followers, setFollowers] = useState(mockFollowers)
   const [repos, setRepos] = useState(mockRepos)
 
+  const [isLoading, setIsLoading] = useState(false)
+  const [request, setRequest] = useState(0)
+  const [error, setError] = useState({ show: false, msg: '' })
+
+  const searchGithubUser = async (user) => {
+    toggleError()
+    setIsLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    )
+    if (response) {
+      setGithubUser(response.data)
+      const { repos_url, followers_url } = response.data
+
+      await Promise.allSettled([
+        axios(`${repos_url}?per_page=100`),
+        axios(`${followers_url}`),
+      ])
+        .then((result) => {
+          const [repos, followers] = result
+          if (repos.status === 'fulfilled') {
+            setRepos(repos.value.data)
+          }
+          if (followers.status === 'fulfilled') {
+            setFollowers(followers.value.data)
+          }
+        })
+        .catch((err) => console.log(err))
+    } else {
+      toggleError(true, 'there is no user with that username')
+    }
+    checkRequest()
+    setIsLoading(false)
+  }
+
+  const checkRequest = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data
+        setRequest(remaining)
+        if (remaining === 0) {
+          toggleError(true, 'sorry, you have exceeded your hourly rate limit!')
+        }
+      })
+      .catch((error) => console.log(error))
+  }
+
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg })
+  }
+
+  useEffect(checkRequest, [])
+
+  useEffect(() => {
+    searchGithubUser('Mahmud1087')
+  }, [])
+
   return (
-    <GithubContext.Provider value={{ githubUser, followers, repos }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        followers,
+        repos,
+        isLoading,
+        searchGithubUser,
+        request,
+        error,
+      }}>
       {children}
     </GithubContext.Provider>
   )
