dc42019fccbafe7d09217782c6263db78e17bdd7
Mahmud Abdulazeez <abdulazeezm578@gmail.com>
2023-01-19 23:18:28 +0100

    fetched products

--- react-comfy-ecommerce/src/context/products_context.jsx
+++ react-comfy-ecommerce/src/context/products_context.jsx
@@ -15,6 +15,12 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  single_products_loading: false,
+  single_products_error: false,
+  single_products: [],
 }
 
 const ProductsContext = React.createContext()
@@ -30,12 +36,24 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE })
   }
 
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
+  }
+
   return (
     <ProductsContext.Provider
       value={{
         ...state,
         sideBarOpen,
         sideBarClose,
+        fetchProducts,
       }}>
       {children}
     </ProductsContext.Provider>
